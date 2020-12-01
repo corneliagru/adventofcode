07be55cd8dc4defc22391418babc02e3232716b5
Cornelia Gruber <cornelia.gruber@campus.lmu.de>
2020-12-01 14:44:10 +0200

    day 1

---
+++ day1.R
@@ -0,0 +1,27 @@
+input1 <- read.table("~/R/adventofcode/input1.txt", quote = "\"", comment.char = "")
+
+
+# add 2 nums --------------------------------------------------------------
+
+for (i in 1:length(input1$V1)) {
+     for (j in 1:length(input1$V1)) {
+         if (input1[i,1] + input1[j,1] == 2020){
+           print(c(input1[i,1], input1[j,1], input1[i,1] * input1[j,1]))
+         }
+       }
+   }
+
+# for 3 nums --------------------------------------------------------------
+
+for (i in 1:length(input1$V1)) {
+  for (j in 1:length(input1$V1)) {
+    for (k in 1:length(input1$V1)) {
+      if (input1[i,1] + input1[j,1] > 2020) break
+      if (input1[i,1] + input1[j,1] + input1[k,1] == 2020)
+      {
+        print(c(input1[i,1], input1[j,1], input1[k,1],
+                input1[i,1] * input1[j,1] * input1[k,1]))
+      }
+    }
+  }
+}
